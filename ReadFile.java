b3c9c27cb59eea2214fa82e7e4fced5bf2799282
carl <carl.last@gmail.com>
2021-02-05 22:19:57 +0000

    Added Tilde install script

--- ReadFile.java
+++ ReadFile.java
@@ -1,9 +1,6 @@
-import java.io.File;  
-// Import the File class 
-import java.io.FileNotFoundException;  
-// Import this class to handle errors 
-import java.util.Scanner; 
-// Import the Scanner class to read text files  
+import java.io.File;  // Import the File class 
+import java.io.FileNotFoundException;  // Import this class to handle errors 
+import java.util.Scanner; // Import the Scanner class to read text files  
 public class ReadFile {   
 public static void main(String[] args) {
      try {
---
+++ install_tilde.sh
@@ -0,0 +1,2 @@
+# Excellent console based editor
+wget http://os.ghalkes.nl/sources.list.d/install_repo.sh ; sudo sh ./install_repo.sh ; sudo apt-get install tilde
